6257717417e49e74a8ad247166f896068a56dcd8
Ilya Beliaev <info@php-dev.info>
2026-04-12 14:51:02 +0200

    initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\LaravelWebdavServer\Tests;
+
+class ExampleTest extends TestCase
+{
+    public function testExample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
