4ed4fc7fd22de4142721719f4dc8f44c1b0d86b7
Ilya Beliaev <info@php-dev.info>
2026-04-12 14:52:00 +0200

    initial commit

---
+++ tests/DatabaseTestCase.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\LaravelWebdavServer\Tests;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Orchestra\Testbench\Attributes\WithMigration;
+
+#[WithMigration]
+class DatabaseTestCase extends TestCase
+{
+    use LazilyRefreshDatabase;
+
+
+    protected function defineEnvironment($app): void
+    {
+        $config = $app->make(Repository::class);
+
+        $config->set([
+            'database.default' => 'testing',
+            'database.connections.testing' => [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+                'foreign_key_constraints' => false,
+            ],
+        ]);
+    }
+}
