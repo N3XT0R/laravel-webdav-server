1b9129b51ecfbe5d201d448a742f7f626542c97d
Ilya Beliaev <info@php-dev.info>
2026-04-12 18:58:48 +0200

    initial commit

--- src/LaravelWebdavServerServiceProvider.php
+++ src/LaravelWebdavServerServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace N3XT0R\LaravelWebdavServer;
 
+use N3XT0R\LaravelWebdavServer\Auth\Backends\BasicAuthBackend;
 use N3XT0R\LaravelWebdavServer\Auth\Validators\DatabaseCredentialValidator;
 use N3XT0R\LaravelWebdavServer\Commands\LaravelWebdavServerCommand;
 use N3XT0R\LaravelWebdavServer\Contracts\Auth\CredentialValidatorInterface;
 use N3XT0R\LaravelWebdavServer\Contracts\Repositories\WebDavAccountRepositoryInterface;
-use N3XT0R\LaravelWebdavServer\Http\Server\WebDavServerFactory;
+use N3XT0R\LaravelWebdavServer\Server\WebDavServerFactory;
 use N3XT0R\LaravelWebdavServer\Repositories\EloquentWebDavAccountRepository;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -35,8 +36,10 @@ class LaravelWebdavServerServiceProvider extends PackageServiceProvider
 
     public function packageRegistered(): void
     {
-        $this->app->scoped(WebDavServerFactory::class, function ($app) {
-            return new WebDavServerFactory($app);
+        $this->app->singleton(WebDavServerFactory::class, function ($app): WebDavServerFactory {
+            return new WebDavServerFactory(
+                authBackend: $app->make(BasicAuthBackend::class),
+            );
         });
     }
 
