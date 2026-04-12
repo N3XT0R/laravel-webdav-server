60c885b12909505e6bf274ce399c78ce539aa0da
Ilya Beliaev <info@php-dev.info>
2026-04-12 18:29:51 +0200

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use N3xt0r\LaravelWebdavServer\Http\Controllers\WebDavController;
+use N3XT0R\LaravelWebdavServer\Http\Controllers\WebDavController;
 
 Route::any('/webdav/{path?}', WebDavController::class)
     ->where('path', '.*');
