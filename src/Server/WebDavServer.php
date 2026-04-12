bd95b2cc0a4988fded062d95570c265f2e972b81
Ilya Beliaev <info@php-dev.info>
2026-04-12 18:57:01 +0200

    initial commit

---
+++ src/Server/WebDavServer.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\LaravelWebdavServer\Server;
+
+use N3XT0R\LaravelWebdavServer\Auth\BasicAuthBackend;
+use Sabre\DAV\Auth\Plugin as AuthPlugin;
+use Sabre\DAV\Server;
+use Sabre\DAV\SimpleCollection;
+
+final readonly class WebDavServer
+{
+    public function __construct(
+        private BasicAuthBackend $authBackend,
+        private string $baseUri = '/webdav/',
+    ) {
+    }
+
+    public function create(): Server
+    {
+        $root = new SimpleCollection('root');
+        $server = new Server($root);
+
+        $server->setBaseUri($this->baseUri);
+        $server->addPlugin(new AuthPlugin($this->authBackend));
+
+        return $server;
+    }
+}
---
+++ src/Server/WebDavServerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\LaravelWebdavServer\Server;
+
+use N3XT0R\LaravelWebdavServer\Auth\BasicAuthBackend;
+
+final readonly class WebDavServerFactory
+{
+    public function __construct(
+        private BasicAuthBackend $authBackend,
+    ) {
+    }
+
+    public function make(): WebDavServer
+    {
+        return new WebDavServer(
+            authBackend: $this->authBackend,
+            baseUri: (string)config('webdav.base_uri', '/webdav/'),
+        );
+    }
+}
