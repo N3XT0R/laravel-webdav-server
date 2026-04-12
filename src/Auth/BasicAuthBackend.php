8d832bd993db7b31e8c2ae84eee864704e121a6a
Ilya Beliaev <info@php-dev.info>
2026-04-12 18:34:33 +0200

    initial commit

--- src/Auth/BasicAuthBackend.php
+++ src/Auth/BasicAuthBackend.php
@@ -4,7 +4,43 @@ declare(strict_types=1);
 
 namespace N3XT0R\LaravelWebdavServer\Auth;
 
-class BasicAuthBackend
-{
+use N3XT0R\LaravelWebdavServer\Contracts\CredentialValidatorInterface;
+use N3XT0R\LaravelWebdavServer\ValueObjects\WebDavPrincipal;
+use Sabre\DAV\Auth\Backend\AbstractBasic;
 
+class BasicAuthBackend extends AbstractBasic
+{
+    private ?WebDavPrincipal $principal = null;
+
+    public function __construct(
+        protected readonly CredentialValidatorInterface $validator,
+        protected $realm = 'Laravel WebDAV',
+    ) {
+    }
+
+    protected function validateUserPass($username, $password): bool
+    {
+        $principal = $this->validator->validate($username, $password);
+
+        if (!$principal) {
+            return false;
+        }
+
+        $this->principal = $principal;
+
+        return true;
+    }
+
+    /**
+     * Expose authenticated principal for later use (e.g. space resolver).
+     */
+    public function getPrincipal(): ?WebDavPrincipal
+    {
+        return $this->principal;
+    }
+
+    public function getRealm(): string
+    {
+        return $this->realm;
+    }
 }
