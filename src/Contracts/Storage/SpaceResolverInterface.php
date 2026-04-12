c58d8f148146e7d7584ce2eb999e4e69c8f432a8
Ilya Beliaev <info@php-dev.info>
2026-04-12 19:10:10 +0200

    initial commit

--- src/Contracts/Storage/SpaceResolverInterface.php
+++ src/Contracts/Storage/SpaceResolverInterface.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace N3XT0R\LaravelWebdavServer\Contracts\Storage;
 
+use N3XT0R\LaravelWebdavServer\Storage\Data\WebDavStorageSpace;
+use N3XT0R\LaravelWebdavServer\ValueObjects\WebDavPrincipal;
+
 interface SpaceResolverInterface
 {
+    public function resolve(WebDavPrincipal $principal): WebDavStorageSpace;
 }
