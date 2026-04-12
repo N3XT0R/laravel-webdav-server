2db6cb94cb1bbaf895b2ecc762c85b88be1218cf
Ilya Beliaev <info@php-dev.info>
2026-04-12 18:27:48 +0200

    initial commit

--- src/Http/Server/WebDavServer.php
+++ src/Http/Server/WebDavServer.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace N3XT0R\LaravelWebdavServer\Http\Server;
 
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class WebDavServer
 {
-
+    public function handle(Request $request): Response
+    {
+        return new Response('WebDAV bootstrap placeholder', 200);
+    }
 }
--- src/Http/Server/WebDavServerFactory.php
+++ src/Http/Server/WebDavServerFactory.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace N3XT0R\LaravelWebdavServer\Http\Server;
 
-class WebDavServerFactory
-{
+use Illuminate\Contracts\Container\Container;
 
+readonly class WebDavServerFactory
+{
+    public function __construct(
+        private Container $container,
+    ) {
+    }
+
+    public function make(): WebDavServer
+    {
+        return new WebDavServer();
+    }
 }
