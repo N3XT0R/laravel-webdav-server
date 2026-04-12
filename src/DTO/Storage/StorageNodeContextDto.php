30f69df155acee022d07977fb4bc66c9556a41fa
Ilya Beliaev <info@php-dev.info>
2026-04-12 21:51:30 +0200

    initial commit

---
+++ src/DTO/Storage/StorageNodeContextDto.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\LaravelWebdavServer\DTO\Storage;
+
+use Illuminate\Contracts\Filesystem\Factory as FilesystemManager;
+use N3XT0R\LaravelWebdavServer\Contracts\Auth\PathAuthorizationInterface;
+use N3XT0R\LaravelWebdavServer\ValueObjects\WebDavPrincipal;
+
+final readonly class StorageNodeContextDto
+{
+    public function __construct(
+        public string $disk,
+        public FilesystemManager $filesystem,
+        public WebDavPrincipal $principal,
+        public PathAuthorizationInterface $authorization,
+    ) {
+    }
+}
