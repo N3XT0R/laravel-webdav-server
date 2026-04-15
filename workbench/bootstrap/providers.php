16cf040985f8ec5e5e9f79f562bfc8411d60993b
Ilya Beliaev <info@php-dev.info>
2026-04-15 20:22:49 +0200

    initial commit

---
+++ workbench/bootstrap/providers.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | WebDav Server Providers
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the service providers for the WebDav server. These
+    | providers will be registered when the server is bootstrapped.
+    |
+    */
+
+    N3XT0R\LaravelWebdavServer\Providers\LaravelWebdavServerServiceProvider::class,
+];
