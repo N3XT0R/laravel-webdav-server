880cb3e3f50e161c21468e7b0d40925c0aeaa71f
Ilya Beliaev <info@php-dev.info>
2026-04-19 00:31:58 +0200

    feat: implementing package features

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,17 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use N3XT0R\LaravelWebdavServer\Http\Controllers\WebDavController;
 
-Route::any('/webdav/{space}/{path?}', WebDavController::class)
-    ->where('path', '.*');
+Route::match([
+    'OPTIONS',
+    'GET',
+    'HEAD',
+    'PUT',
+    'DELETE',
+    'PROPFIND',
+    'PROPPATCH',
+    'MKCOL',
+    'COPY',
+    'MOVE',
+    'LOCK',
+    'UNLOCK',
+], '/webdav/{path?}', WebDavController::class)->where('path', '.*');
