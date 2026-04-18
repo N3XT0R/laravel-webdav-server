fd8bfe213f2df4bc245b39a0755ddf6ba81ea68e
Ilya Beliaev <info@php-dev.info>
2026-04-19 00:15:26 +0200

    feat: implementing package features

--- src/Exception/Auth/MissingCredentialsException.php
+++ src/Exception/Auth/MissingCredentialsException.php
@@ -5,9 +5,8 @@ declare(strict_types=1);
 namespace N3XT0R\LaravelWebdavServer\Exception\Auth;
 
 use N3XT0R\LaravelWebdavServer\Exception\DomainException;
-use Illuminate\Contracts\Debug\ShouldntReport;
 
-class MissingCredentialsException extends DomainException implements ShouldntReport
+class MissingCredentialsException extends DomainException
 {
 
 }
