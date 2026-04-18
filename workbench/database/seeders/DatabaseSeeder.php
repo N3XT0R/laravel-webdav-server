8954dcbb4b89e11e91faefdc647523af66821baa
Ilya Beliaev <info@php-dev.info>
2026-04-18 23:38:30 +0200

    feat: implementing package features

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use N3XT0R\LaravelWebdavServer\Models\WebDavAccount;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,13 +19,22 @@ class DatabaseSeeder extends Seeder
         $password = 'password';
 
         $factory = UserFactory::new();
+        $webdavAccountFactory = WebDavAccount::factory()
+            ->withUserName('testuser');
         if (app()->runningInConsole()) {
-            $factory->withPassword('password');
+            $factory->withPassword($password);
+            $webdavAccountFactory->withPassword($password);
         }
 
         $user = $factory->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $webdavAccountFactory
+            ->withUserId($user->getKey())
+            ->create([
+                'display_name' => 'Test User',
+            ]);
     }
 }
