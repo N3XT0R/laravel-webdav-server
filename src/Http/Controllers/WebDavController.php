8460f02e328c89aeefb2d48634a7dd2be344c828
Ilya Beliaev <info@php-dev.info>
2026-04-19 00:25:35 +0200

    feat: implementing package features

--- src/Http/Controllers/WebDavController.php
+++ src/Http/Controllers/WebDavController.php
@@ -13,11 +13,12 @@ final class WebDavController extends Controller
 {
     public function __construct(
         private readonly WebDavServerFactory $factory,
-    ) {}
+    ) {
+    }
 
     public function __invoke(Request $request): Response
     {
-        if (! $this->hasBasicAuthAttempt($request)) {
+        if (!$this->hasBasicAuthAttempt($request)) {
             return response('Unauthorized', Response::HTTP_UNAUTHORIZED, [
                 'WWW-Authenticate' => 'Basic realm="WebDAV"',
             ]);
@@ -28,13 +29,15 @@ final class WebDavController extends Controller
 
         try {
             $server->start();
-            $content = (string) ob_get_clean();
+            $content = (string)ob_get_clean();
 
             return response($content);
         } catch (\Throwable $e) {
             throw $e;
         } finally {
-            ob_end_clean();
+            if (ob_get_level() > 0) {
+                ob_end_clean();
+            }
         }
     }
 
