b524f314f1f18f2ee4fd141b6d33c6db3e41cc25
Ilya Beliaev <info@php-dev.info>
2026-04-19 17:48:36 +0200

    feat: implementing package features

--- src/DTO/Storage/StorageNodeContextDto.php
+++ src/DTO/Storage/StorageNodeContextDto.php
@@ -1,9 +1,13 @@
 <?php
+
 declare(strict_types=1);
+
 namespace N3XT0R\LaravelWebdavServer\DTO\Storage;
+
 use Illuminate\Contracts\Filesystem\Filesystem;
 use N3XT0R\LaravelWebdavServer\Contracts\Auth\PathAuthorizationInterface;
 use N3XT0R\LaravelWebdavServer\ValueObjects\WebDavPrincipal;
+
 final readonly class StorageNodeContextDto
 {
     public function __construct(
--- src/Server/Storage/StorageRootBuilder.php
+++ src/Server/Storage/StorageRootBuilder.php
@@ -1,6 +1,9 @@
 <?php
+
 declare(strict_types=1);
+
 namespace N3XT0R\LaravelWebdavServer\Server\Storage;
+
 use Illuminate\Contracts\Filesystem\Factory as FilesystemManager;
 use N3XT0R\LaravelWebdavServer\Contracts\Auth\PathAuthorizationInterface;
 use N3XT0R\LaravelWebdavServer\Contracts\Server\StorageRootBuilderInterface;
@@ -8,12 +11,14 @@ use N3XT0R\LaravelWebdavServer\DTO\Storage\StorageNodeContextDto;
 use N3XT0R\LaravelWebdavServer\Nodes\StorageRootCollection;
 use N3XT0R\LaravelWebdavServer\Storage\Data\WebDavStorageSpace;
 use N3XT0R\LaravelWebdavServer\ValueObjects\WebDavPrincipal;
+
 final readonly class StorageRootBuilder implements StorageRootBuilderInterface
 {
     public function __construct(
         private PathAuthorizationInterface $authorization,
         private FilesystemManager $filesystem,
     ) {}
+
     public function build(WebDavPrincipal $principal, WebDavStorageSpace $space): StorageRootCollection
     {
         return new StorageRootCollection(
